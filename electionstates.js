243a693b037d37a3f124b5fdf3188ca88112bf40
sikB <sikhovski@icloud.com>
2016-04-07 16:46:10 -0400

    vote bars updates

--- electionstates.js
+++ electionstates.js
@@ -2,6 +2,7 @@ var interactiveMapApp =angular.module('interactiveMapApp', []);
 interactiveMapApp.controller('interactiveMapCtrl', function($scope){
 	resetStates();
 	$scope.states = states;
+	calculateStateTotals();
 
 	$scope.stateClicked = function(state){
 		var newColor = getNewColor(state);
@@ -16,5 +17,23 @@ interactiveMapApp.controller('interactiveMapCtrl', function($scope){
 		}else{
 			return "you in the green party homes??";
 		}
+	}
+
+	 function calculateStateTotals(){
+		$scope.redStateVotes = 0;
+		$scope.blueStateVotes = 0;
+		$scope.openStateVotes = 0;
+		for(i=0; i<numStates; i++){
+			if(blueStates[i]){
+				$scope.blueStateVotes+= blueStates[i].electoralVotes;
+			}else if(redStates[i]){
+				$scope.redStateVotes+= redStates[i].electoralVotes;
+			}else if(openStates[i]){
+				$scope.openStateVotes+= openStates[i].electoralVotes;
+			}
+		}
+		$scope.blueWidth = $scope.blueStateVotes / 538 * 100 + "%";
+		$scope.redWidth = $scope.redStateVotes / 538 * 100 + "%";
+		$scope.openWidth = $scope.openStateVotes / 538 * 100 + "%";
 	}
 });
